547db3ed1e320d248c15a1fd03da80ce6448d6be
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-11-10 15:39:41 +0700

    Add test_qiskit file

---
+++ test_qiskit.py
@@ -0,0 +1,11 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator 
+
+qc = QuantumCircuit(1)
+qc.h(0)
+qc.measure_all()
+
+sim = AerSimulator()
+result = sim.run(qc).result()
+print(result.get_counts())
+
